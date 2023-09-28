2f94898bda892ccec631456ae0f3cae3756f1b22
Marcos <129004971+RantaroMM@users.noreply.github.com>
2023-09-28 19:15:26 -0300

    Add files via upload

---
+++ Piloto.cpp
@@ -0,0 +1,24 @@
+#include "Piloto.h"
+#include "Amplificador.h"
+#include "Integrador.h"
+
+Piloto::Piloto(double ganho){
+    this->ganho = ganho;
+}
+
+Sinal* Piloto::processar(Sinal *sinalIN){
+    Amplificador* Gain = new Amplificador(ganho);
+    Sinal* sinalOUT1 = new Sinal(sinalIN->getSequencia(), sinalIN->getComprimento());
+    
+    sinalOUT1 = Gain->processar(sinalIN);
+
+    Integrador* Integral = new Integrador();
+    Sinal* sinalOUT2 = new Sinal(sinalOUT1->getSequencia(), sinalOUT1->getComprimento());
+
+    sinalOUT2 = Integral->processar(sinalOUT1);
+
+    return sinalOUT2;
+}
+
+Piloto::~Piloto(){
+}
